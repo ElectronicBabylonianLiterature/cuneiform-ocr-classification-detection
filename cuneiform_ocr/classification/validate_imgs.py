d9037888752fdafdecd16434fdaaa6ffd38e30fb
Yunus Cobanoglu <56864644+yCobanoglu@users.noreply.github.com>
2023-12-11 19:08:24 +0100

    Update validate_imgs.py

--- cuneiform_ocr/classification/validate_imgs.py
+++ cuneiform_ocr/classification/validate_imgs.py
@@ -6,30 +6,29 @@ mmcv.use_backend("pillow")
 from PIL import Image
 
 def _validate_img(file):
-        try:
-                    img = Image.open(file)
-                            if mmcv.imread(file) is None:
-                                            print(f"Could not read {file}")
-                                                    k = mmcv.imfrombytes(open(file, "rb").read())
-                                                            if k is None:
-                                                                            print(f"Could not read {file}")
-                                                                                    img.verify()
-                                                                                            img.close()
-                                                                                                except (IOError, SyntaxError):
-                                                                                                            print("Bad file:", file)
-                                                                                                                    os.remove(file)
+    try:
+        img = Image.open(file)
+        if mmcv.imread(file) is None:
+            print(f"Could not read {file}")
+        k = mmcv.imfrombytes(open(file, "rb").read())
+        if k is None:
+            print(f"Could not read {file}")
+        img.verify()
+        img.close()
+    except (IOError, SyntaxError):
+        print("Bad file:", file)
+        os.remove(file)
 
 
-                                                                                                                    def validate_imgs(imgs_path: Path):
-                                                                                                                            for dir in imgs_path.iterdir():
-                                                                                                                                        if dir.is_dir():
-                                                                                                                                                        for sign_dir in dir.iterdir():
-                                                                                                                                                                            for dir in sign_dir.iterdir():
-                                                                                                                                                                                                    for file in dir.iterdir():
-                                                                                                                                                                                                                                _validate_img(file)
+def validate_imgs(imgs_path: Path):
+    for dir in imgs_path.iterdir():
+        if dir.is_dir():
+            for sign_dir in dir.iterdir():
+                for dir in sign_dir.iterdir():
+                    for file in dir.iterdir():
+                        _validate_img(file)
 
-                                                                                                                                                                                                                                if __name__ == "__main__":
-                                                                                                                                                                                                                                        # can be used to manually validate files and delete bad ones for classification
-                                                                                                                                                                                                                                            imgs_path = Path("cuneiform_ocr_data/classification/data/ebl")
-                                                                                                                                                                                                                                                validate_imgs(imgs_path)
-
+if __name__ == "__main__":
+    # can be used to manually validate files and delete bad ones for classification
+    imgs_path = Path("cuneiform_ocr_data/classification/data/ebl")
+    validate_imgs(imgs_path)
